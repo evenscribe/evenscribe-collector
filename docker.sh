b4c9ac0ec882ceab74a624bf9be76e1740e5f1a0
Sangit Manandhar <rux12@icloud.com>
2024-05-08 16:08:46 -0400

    feat: update docker.sh to be more functional

--- docker.sh
+++ docker.sh
@@ -1,45 +1,58 @@
 #/bin/sh
 echo "Initiating Docker Sequence...."
 
-path="/tmp/clickhouse"
-data_path="$path/data"
-logs_path="$path/logs"
-migration_script="./init-db.sh"
+C_PATH="/tmp/clickhouse"
+DATA_PATH="$C_PATH/data"
+LOGS_PATH="$C_PATH/logs"
+MIGRATION_SCRIPT="./init-db.sh"
+CONTAINER_NAME="clickhouse-db"
+IMAGE_NAME="clickhouse/clickhouse-server"
 
 if [ -d $path ]; then
     echo "Mount path exists."
     if [ "$1" = "--clean" ]; then
         echo "Cleaning mount path...."
-        rm -rf $data_path/* $logs_path/*
+        rm -rf $DATA_PATH/* $LOGS_PATH/*
     fi
 else
     echo "Creating mount path...."
-    mkdir $path
-    mkdir $data_path $logs_path
+    mkdir $C_PATH
+    mkdir $DATA_PATH $LOGS_PATH
 fi
 
-CONTAINER_NAME="clickhouse-db"
+if docker info > /dev/null 2>&1; then
+    echo "Docker is running..."
+else
+    echo "Starting Docker...."
+    open --background -a Docker
+    sleep 3
+fi
 
 if [ -n "$(docker ps -f "name=$CONTAINER_NAME" -f "status=running" -q)" ]; then
     echo "The container '$CONTAINER_NAME' is already running.\n"
 else
-    if docker ps -a | grep -q "$CONTAINER_NAME"; then
-        echo "Container $CONTAINER_NAME exists."
-        echo "Running $CONTAINER_NAME ...."
-        docker start $CONTAINER_NAME >/dev/null 2>&1
-        echo "$CONTAINER_NAME started.\n"
-    else
-        echo "Container $CONTAINER_NAME does not exist."
-        echo "Creating $CONTAINER_NAME ...."
-        docker run -d \
-            -v $(realpath $data_path):/var/lib/clickhouse/ \
-            -v $(realpath $logs_path):/var/log/clickhouse-server/ \
-            -v $(realpath $migration_script):/docker-entrypoint-initdb.d/init-db.sh \
-            -p 19000:9000 \
-            -p 8123:8123 \
-            --name $CONTAINER_NAME \
-            --ulimit nofile=262144:262144 \
-            clickhouse/clickhouse-server >/dev/null 2>&1
-        echo "$CONTAINER_NAME started.\n"
-    fi
+        if docker ps -a | grep -q "$CONTAINER_NAME"; then
+            echo "Container $CONTAINER_NAME exists."
+            echo "Running $CONTAINER_NAME ...."
+            docker start $CONTAINER_NAME >/dev/null 2>&1
+            echo "$CONTAINER_NAME started.\n"
+        else
+            if docker image inspect "$IMAGE_NAME" &> /dev/null; then
+                echo "The image $IMAGE_NAME exists."
+            else
+                docker pull $IMAGE_NAME
+            fi
+            echo "Container $CONTAINER_NAME does not exist."
+            echo "Creating $CONTAINER_NAME ...."
+            docker run -d \
+                -v $(realpath $DATA_PATH):/var/lib/clickhouse/ \
+                -v $(realpath $LOGS_PATH):/var/log/clickhouse-server/ \
+                -v $(realpath $MIGRATION_SCRIPT):/docker-entrypoint-initdb.d/init-db.sh \
+                -p 19000:9000 \
+                -p 8123:8123 \
+                --name $CONTAINER_NAME \
+                --ulimit nofile=262144:262144 \
+                clickhouse/clickhouse-server >/dev/null 2>&1
+            echo "$CONTAINER_NAME started.\n"
+        fi
 fi
