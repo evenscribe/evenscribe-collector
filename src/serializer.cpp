83dd24010807b5e671f7212ba765eb4e2bfd6226
Sangit Manandhar <rux12@icloud.com>
2024-05-13 14:16:57 -0400

    feat: add LogEntry struct serializer

--- src/serializer.cpp
+++ src/serializer.cpp
@@ -1,14 +1,45 @@
+#ifndef SERIALIZER
+#define SERIALIZER
+
+#include <iostream>
 #include <nlohmann/json.hpp>
+#include <string>
 
 using json = nlohmann::json;
 
-/* Log { */
-/*   level; */
-/*   timestamp; */
-/*   source; */
-/*   message; */
-/*   context; */
-/* }; */
+struct LogOwner {
+  std::string host_name;
+  std::string app_name;
+};
+
+struct Log {
+  std::string level;
+  std::string message;
+};
+
+struct LogEntry {
+  int timestamp;
+  LogOwner log_owner;
+  Log log;
+  std::string _msg;
+};
+
+inline void from_json(const json &j, LogOwner &log_owner) {
+  j.at("host_name").get_to(log_owner.host_name);
+  j.at("app_name").get_to(log_owner.app_name);
+}
+
+inline void from_json(const json &j, Log &log) {
+  j.at("level").get_to(log.level);
+  j.at("message").get_to(log.message);
+}
+
+inline void from_json(const json &j, LogEntry &log_entry) {
+  j.at("@timestamp").get_to(log_entry.timestamp);
+  j.at("_msg").get_to(log_entry._msg);
+  j.at("log").get_to(log_entry.log);
+  j.at("log_owner").get_to(log_entry.log_owner);
+}
 
 class Serializer {
 
@@ -23,8 +54,11 @@ private:
   }
 
 public:
-  static json serialize(std::string source) {
+  static LogEntry serialize(std::string source) {
     json parsed = json::parse(trim(source));
+    std::cout << parsed.dump(4);
     return parsed;
   }
 };
+
+#endif // !SERIALIZER
