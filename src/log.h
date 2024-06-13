a123f3408c7641459bfd171ef27f42e6618daea0
Sangit Manandhar <rux12@icloud.com>
2024-06-13 11:17:03 -0400

    chore: clean log.h

--- src/log.h
+++ src/log.h
@@ -4,17 +4,17 @@
 #include <cstdarg>
 #include <cstdio>
 #include <cstdlib>
-extern bool g_verbose;
+// extern bool g_verbose;
 
-static inline void debug(const char *format, ...) {
-  if (!g_verbose)
-    return;
+// static inline void debug(const char *format, ...) {
+//   if (!g_verbose)
+//     return;
 
-  va_list args;
-  va_start(args, format);
-  vfprintf(stdout, format, args);
-  va_end(args);
-}
+//   va_list args;
+//   va_start(args, format);
+//   vfprintf(stdout, format, args);
+//   va_end(args);
+// }
 
 static inline void warn(const char *format, ...) {
   va_list args;
@@ -39,27 +39,27 @@ static inline void error(const char *format, ...) {
   exit(EXIT_FAILURE);
 }
 
-static inline void require(const char *format, ...) {
-  va_list args;
-  va_start(args, format);
-  vfprintf(stderr, format, args);
-  va_end(args);
+// static inline void require(const char *format, ...) {
+//   va_list args;
+//   va_start(args, format);
+//   vfprintf(stderr, format, args);
+//   va_end(args);
 
-  exit(EXIT_SUCCESS);
-}
+//   exit(EXIT_SUCCESS);
+// }
 
-static inline void debug_message(const char *prefix, char *message) {
-  if (!g_verbose)
-    return;
+// static inline void debug_message(const char *prefix, char *message) {
+//   if (!g_verbose)
+//     return;
 
-  fprintf(stdout, "%s:", prefix);
+//   fprintf(stdout, "%s:", prefix);
 
-  for (; *message;) {
-    message += fprintf(stdout, " %s", message);
-  }
+//   for (; *message;) {
+//     message += fprintf(stdout, " %s", message);
+//   }
 
-  putc('\n', stdout);
-  fflush(stdout);
-}
+//   putc('\n', stdout);
+//   fflush(stdout);
+// }
 
 #endif // !Log
