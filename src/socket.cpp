29d474e17004182ba413165314acc1480091f58f
Sangit Manandhar <rux12@icloud.com>
2024-06-20 00:55:19 -0400

    feat: change to two thread channels one thread channel -> handles connections other thread channel -> handles database saves

--- src/socket.cpp
+++ src/socket.cpp
@@ -1,6 +1,5 @@
 #include "socket.h"
 #include "clickhouse_query_generator.cpp"
-#include "helper.h"
 #include "log.h"
 #include "param.h"
 #include "postgres_query_generator.cpp"
@@ -11,7 +10,6 @@
 #include <cstring>
 #include <ctime>
 #include <iostream>
-#include <mutex>
 #include <queue>
 #include <string.h>
 #include <string>
@@ -20,11 +18,6 @@
 #include <unistd.h>
 #include <vector>
 #include <victorialogslib/client.h>
-
-//
-std::vector<std::vector<std::tuple<std::string, time_t>>>
-    insert_statements(THREADS);
-//
 
 //
 ClickhouseQueryGenerator clickhouse_query_generator;
@@ -49,13 +42,21 @@ typedef struct {
   int thread_id;
 } connection_t;
 
-std::mutex mtx;
-pthread_t thread_pool[THREADS];
+pthread_t thread_pool_conn[THREADS];
 std::queue<connection_t *> task_queue;
 
 pthread_mutex_t queue_mtx = PTHREAD_MUTEX_INITIALIZER;
 pthread_cond_t cond_var = PTHREAD_COND_INITIALIZER;
 bool done = false;
+
+//
+std::vector<std::tuple<std::string, time_t>> insert_statements;
+pthread_t thread_pool_inserts[THREADS];
+
+pthread_mutex_t inserts_mtx = PTHREAD_MUTEX_INITIALIZER;
+pthread_cond_t inserts_cond_var = PTHREAD_COND_INITIALIZER;
+bool done_inserts = false;
+//
 
 void process_clickhouse(void *arg) {
   connection_t *conn = (connection_t *)arg;
@@ -148,6 +149,23 @@ std::string read_socket(int socket_fd) {
   return sockResponseString;
 }
 
+bool is_socket_open(int sockfd) {
+  char buffer;
+  ssize_t result = recv(sockfd, &buffer, 1, MSG_PEEK);
+
+  if (result == 0) {
+    return false;
+  } else if (result < 0) {
+    if (errno == EAGAIN || errno == EWOULDBLOCK) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  return true;
+}
+
 void process_postgres(void *arg) {
   connection_t *conn = (connection_t *)arg;
 
@@ -155,41 +173,21 @@ void process_postgres(void *arg) {
     return;
   }
 
-  std::string buf = read_socket(conn->client_socket);
+  char buf[BUFFER_SIZE];
 
-  Log entry = Serializer::serialize(buf);
-  if (entry.is_vaild) {
-    std::string query = postgres_query_generator.create_query(entry);
-    insert_statements[conn->thread_id].push_back(
-        std::make_tuple(query, time(NULL)));
+  while (is_socket_open(conn->client_socket) &&
+         read(conn->client_socket, buf, BUFFER_SIZE) > 0) {
 
-    std::string query_string =
-        get_query_from_bucket(insert_statements[conn->thread_id]);
-
-    if (insert_statements[conn->thread_id].size() == 18) {
-      const char *response;
-      try {
-        postgres_db[conn->thread_id]->execute(query_string);
-        response = "OK";
-      } catch (const std::exception &e) {
-        warn(e.what());
-        response = "NO";
-      }
-      // if (write(conn->client_socket, response, sizeof(char) * 2) == -1) {
-      //   warn("Write error.");
-      // }
-      info("Save success.\n");
-      insert_statements[conn->thread_id].clear();
+    Log entry = Serializer::serialize(buf);
+    if (entry.is_vaild) {
+      std::string query = postgres_query_generator.create_query(entry);
+      pthread_mutex_lock(&inserts_mtx);
+      insert_statements.push_back(std::make_tuple(query, time(NULL)));
+      pthread_cond_broadcast(&inserts_cond_var);
+      pthread_mutex_unlock(&inserts_mtx);
     }
   }
-  // }
 
-  // else {
-  // warn("Socket buffer read error.");
-  // }
-  //
-
-  close(conn->client_socket);
   delete conn;
 }
 
@@ -248,6 +246,41 @@ void *worker_postgres(void *arg) {
   pthread_exit(nullptr);
 }
 
+const uint16_t SAVE_THRESHOLD = 1000;
+
+void *worker_postgres_db(void *arg) {
+  while (true) {
+    int index = *((int *)arg);
+    pthread_mutex_lock(&inserts_mtx);
+
+    while (insert_statements.size() < SAVE_THRESHOLD && !done_inserts) {
+      pthread_cond_wait(&inserts_cond_var, &inserts_mtx);
+    }
+
+    if (done_inserts && insert_statements.size() < SAVE_THRESHOLD) {
+      pthread_mutex_unlock(&inserts_mtx);
+      break;
+    }
+
+    std::vector<std::tuple<std::string, time_t>> thread_copy(
+        insert_statements.begin(), insert_statements.begin() + SAVE_THRESHOLD);
+    insert_statements.erase(insert_statements.begin(),
+                            insert_statements.begin() + SAVE_THRESHOLD);
+    pthread_mutex_unlock(&inserts_mtx);
+
+    std::string query_string = get_query_from_bucket(thread_copy);
+    try {
+      postgres_db[index]->execute(query_string);
+    } catch (const std::exception &e) {
+      warn(e.what());
+    }
+    info("Save success.\n");
+  }
+
+  free(arg);
+  pthread_exit(nullptr);
+}
+
 void *worker_victoria(void *arg) {
   while (true) {
     int index = *((int *)arg);
@@ -299,6 +332,7 @@ void initialize_postgres(Config config) {
   std::string db_connection_url =
       "postgres://" + config.user + ":" + config.password + "@" + config.host +
       ":" + std::to_string(config.port) + "/" + config.dbname;
+
   // FIXME: try catch doesn't work here
   // I tried a bunch of stuff but nothing did
   // Just letting the app crash and print the error
@@ -309,7 +343,18 @@ void initialize_postgres(Config config) {
   for (i = 0; i < THREADS; ++i) {
     int *a = (int *)malloc(sizeof(int));
     *a = i;
-    if (pthread_create(&thread_pool[i], nullptr, *worker_postgres, a) != 0) {
+    if (pthread_create(&thread_pool_conn[i], nullptr, *worker_postgres, a) !=
+        0) {
+      error("create thread failed");
+    }
+  }
+
+  int j;
+  for (j = 0; j < THREADS; ++j) {
+    int *a = (int *)malloc(sizeof(int));
+    *a = j;
+    if (pthread_create(&thread_pool_conn[j], nullptr, *worker_postgres_db, a) !=
+        0) {
       error("create thread failed");
     }
   }
@@ -327,7 +372,8 @@ void initialize_clickhouse(Config config) {
   for (i = 0; i < THREADS; ++i) {
     int *a = (int *)malloc(sizeof(int));
     *a = i;
-    if (pthread_create(&thread_pool[i], nullptr, *worker_clickhouse, a) != 0) {
+    if (pthread_create(&thread_pool_conn[i], nullptr, *worker_clickhouse, a) !=
+        0) {
       error("create thread failed");
     }
   }
@@ -341,25 +387,26 @@ void initialize_victoria(Config config) {
   for (i = 0; i < THREADS; ++i) {
     int *a = (int *)malloc(sizeof(int));
     *a = i;
-    if (pthread_create(&thread_pool[i], nullptr, *worker_victoria, a) != 0) {
+    if (pthread_create(&thread_pool_conn[i], nullptr, *worker_victoria, a) !=
+        0) {
       error("create thread failed");
     }
   }
 }
 
-void fd_set_nb(int fd) {
-  errno = 0;
-  int flags = fcntl(fd, F_GETFL, 0);
-  if (errno) {
-    error("couldn't set non-blocking");
-  }
-  flags |= O_NONBLOCK;
-  errno = 0;
-  (void)fcntl(fd, F_SETFL, flags);
-  if (errno) {
-    error("couldn't set non-blocking");
-  }
-}
+// void fd_set_nb(int fd) {
+//   errno = 0;
+//   int flags = fcntl(fd, F_GETFL, 0);
+//   if (errno) {
+//     error("couldn't set non-blocking");
+//   }
+//   flags |= O_NONBLOCK;
+//   errno = 0;
+//   (void)fcntl(fd, F_SETFL, flags);
+//   if (errno) {
+//     error("couldn't set non-blocking");
+//   }
+// }
 
 Socket::Socket(Config config) {
   this->config = config;
@@ -372,7 +419,7 @@ Socket::Socket(Config config) {
   _bind();
   _listen();
 
-  fd_set_nb(server_socket);
+  // fd_set_nb(server_socket);
 
   switch (config.database_kind) {
   case POSTGRES: {
@@ -395,7 +442,7 @@ Socket::~Socket() {
   }
 
   for (int i = 0; i < THREADS; ++i) {
-    pthread_join(thread_pool[i], nullptr);
+    pthread_join(thread_pool_conn[i], nullptr);
   }
 
   switch (this->config.database_kind) {
@@ -412,32 +459,30 @@ Socket::~Socket() {
   remove(SOCKET_PATH);
 }
 
-/// 10s before
-/// [a ,  a,  a,  a]
-
 void Socket::handle_message() {
   while (true) {
-    for (auto &bucket : insert_statements) {
-      if (bucket.size() > 0) {
-        bool should_insert = difftime(time(NULL), std::get<1>(bucket[0])) > 10;
-        if (should_insert) {
-          std::string query_string = get_query_from_bucket(bucket);
-          try {
-            postgres_db[0]->execute(query_string);
-          } catch (const std::exception &e) {
-            warn(e.what());
-          }
-          info("Save success auto.\n");
-          bucket.clear();
-        }
-      }
-    }
+    warn("Waiting for connection...\n");
+    // for (auto &bucket : insert_statements) {
+    //   if (bucket.size() > 0) {
+    //     bool should_insert = difftime(time(NULL), std::get<1>(bucket[0])) >
+    //     10; if (should_insert) {
+    //       std::string query_string = get_query_from_bucket(bucket);
+    //       try {
+    //         postgres_db[0]->execute(query_string);
+    //       } catch (const std::exception &e) {
+    //         warn(e.what());
+    //       }
+    //       info("Save success auto.\n");
+    //       bucket.clear();
+    //     }
+    //   }
+    // }
 
     int client_socket = accept(server_socket, NULL, NULL);
 
     if (client_socket == -1) {
-      // warn("accept connection failed");
-      continue;
+      warn("accept connection failed");
+      // continue;
     }
 
     connection_t *conn = new connection_t{client_socket};
